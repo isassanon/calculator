ce87744ff2ab26f6e22b4a2a4d5d6c1108f44806
isassanon <isabelagomesmachado05@gmail.com>
2026-03-03 12:27:45 -0300

    Criando o display

--- calculadorinha/App.js
+++ calculadorinha/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { endAsyncEvent } from 'react-native/Libraries/Performance/Systrace';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+        <View style={styles.displayContainer}>
+          <Text style={styles.displaytext} numberOfLines={1}>
+            1000
+          </Text>
+        </View>
+
+        <View style={styles.row}>
+
+        </View>
     </View>
   );
 }
@@ -13,8 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  displayContainer: {
+    flex: 2.5,
+    justifyContent: 'flex-end' ,
+    alignItems: 'flex-end' ,
+    paddingHorizontal: 20 ,
+    paddingBottom: 20,
+  },
+  displaytext: {
+    color: '#FFFFFF',
+    fontSize: 72,
+    fontWeight : '300',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  }
 });
